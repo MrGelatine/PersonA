05ec43d30831605f1b70852eb3d814928ddc1d6d
MrGelatine <52462529+MrGelatine@users.noreply.github.com>
2024-05-11 23:30:15 +0300

    Prepare database content view

--- Client/app/src/main/java/com/mrgelatine/persona/ui/imagePicker/ImagePickerScreen.kt
+++ Client/app/src/main/java/com/mrgelatine/persona/ui/imagePicker/ImagePickerScreen.kt
@@ -3,9 +3,21 @@ package com.mrgelatine.persona.ui.imagePicker
 import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
@@ -19,6 +31,7 @@ object ImagePickerDestination: NavigationDestination{
     override val titleRes: Int = R.string.image_picker_title
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ImagePickerScreen(
     navigateToImageInfo: () -> Unit,
@@ -42,18 +55,36 @@ fun ImagePickerScreen(
                 navigateToImageInfo()
             }
     })
-    Column {
-        Button(onClick = {imagePickerDialog.launch("image/*")}) {
+    Scaffold(
+        floatingActionButton = {
+            Row {
+                FloatingActionButton(
+                    onClick = {imagePickerDialog.launch("image/*")}
+                ) {
+                    Icon(Icons.Default.Search, contentDescription = null)
+                }
+                FloatingActionButton(
+                    onClick = {
+                        personAFinderViewModel.screenSize = Pair(screenWidth, screenHeight)
+                        personAFinderViewModel.embeddingsSize = 9216
+                        personAFinderViewModel.prepareFaces(10)
+                        navigateToPersonaFinder()
+                    }
+                ) {
+                    Icon(Icons.Default.Face, contentDescription = null)
+                }
+            }
         }
-        Button(onClick = {
-            personAFinderViewModel.screenSize = Pair(screenWidth, screenHeight)
-            personAFinderViewModel.embeddingsSize = 9216
-            personAFinderViewModel.prepareFaces(10)
-            navigateToPersonaFinder()
-        }
-        ) {
+    ) {
+        innerPadding ->
+            Column(modifier= Modifier.padding(innerPadding)) {
+                val pagerState = rememberPagerState(pageCount = {10})
+                VerticalPager(state = pagerState) {
 
-        }
+                }
+            }
+
     }
+
 
 }
