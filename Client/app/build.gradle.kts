d503493d1561e85347d56c0470b0794ad8f9219c
MrGelatine <52462529+MrGelatine@users.noreply.github.com>
2024-05-19 18:07:30 +0300

    Update gradle configurations

--- Client/app/build.gradle.kts
+++ Client/app/build.gradle.kts
@@ -55,22 +55,31 @@ dependencies {
     val nav_version = "2.7.7"
     val room_version = "2.6.1"
 
-    implementation("androidx.room:room-runtime:$room_version")
+    //Room
     annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 
-    implementation("androidx.navigation:navigation-compose:$nav_version")
+    //Coil
     implementation("io.coil-kt:coil-compose:2.5.0")
 
+    //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+
+    //Compose
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -79,8 +88,10 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    //SwitchableCards
     implementation("com.alexstyl.swipeablecard:swipeablecard:0.1.0")
 
+    //Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
 
